28ec03435f66e6aafeca20d29c3c75a88db28bcb
Jordan Williams <jtw0213@gmail.com>
2022-12-14 23:31:52 -0600

    Refactor some code and small updates

--- 2022/src/main/kotlin/day_11/Main.kt
+++ 2022/src/main/kotlin/day_11/Main.kt
@@ -8,19 +8,7 @@ data class Monkey(val items: ArrayDeque<BigInteger>, val operation: String, val 
 }
 
 fun part1(input: List<String>) {
-    val monkeyList: MutableList<Monkey> = mutableListOf()
-    var monkeyStartParse = 0
-    // Each monkey has 6 lines, followed by blank line
-    while (monkeyStartParse < input.size) {
-        // get start list
-        val startItems = input[monkeyStartParse + 1].split(": ")[1].split(", ").map{it.toBigInteger()}.toMutableList()
-        val operation = input[monkeyStartParse + 2].split(" = ")[1]
-        val divisibleByTest = input[monkeyStartParse+3].substringAfterLast(" ").toInt()
-        val ifTrueThrow = input[monkeyStartParse+4].substringAfterLast(" ").toInt()
-        val ifFalseThrow = input[monkeyStartParse+5].substringAfterLast(" ").toInt()
-        monkeyList.add(Monkey(ArrayDeque(startItems), operation, divisibleByTest, ifTrueThrow, ifFalseThrow))
-        monkeyStartParse += 7
-    }
+    val monkeyList: List<Monkey> = parseInput(input)
 
     for (round in 1..20) {
         for (m in monkeyList) {
@@ -45,31 +33,8 @@ fun part1(input: List<String>) {
     println("Most active monkeys had $m1 and $m2 inspections. Answer: ${m1 * m2}")
 }
 
-fun performOperation(old: BigInteger, op: List<String>): BigInteger {
-    val operand = op[1]
-    val num1 = if (op[0] == "old") old else op[0].toBigInteger()
-    val num2 = if (op[2] == "old") old else op[2].toBigInteger()
-    return if (operand == "*") {
-        num1.multiply(num2)
-    } else {
-        num1.add(num2)
-    }
-}
-
 fun part2(input: List<String>) {
-    val monkeyList: MutableList<Monkey> = mutableListOf()
-    var monkeyStartParse = 0
-    // Each monkey has 6 lines, followed by blank line
-    while (monkeyStartParse < input.size) {
-        // get start list
-        val startItems = input[monkeyStartParse + 1].split(": ")[1].split(", ").map{it.toBigInteger()}.toMutableList()
-        val operation = input[monkeyStartParse + 2].split(" = ")[1]
-        val divisibleByTest = input[monkeyStartParse+3].substringAfterLast(" ").toInt()
-        val ifTrueThrow = input[monkeyStartParse+4].substringAfterLast(" ").toInt()
-        val ifFalseThrow = input[monkeyStartParse+5].substringAfterLast(" ").toInt()
-        monkeyList.add(Monkey(ArrayDeque(startItems), operation, divisibleByTest, ifTrueThrow, ifFalseThrow))
-        monkeyStartParse += 7
-    }
+    val monkeyList: List<Monkey> = parseInput(input)
 
     val bigMod = monkeyList.map(Monkey::divisibleByTest).reduce(Int::times)
     for (round in 1..10000) {
@@ -92,6 +57,35 @@ fun part2(input: List<String>) {
     }
     val (m1, m2) = monkeyList.sortedBy { it.inspections }.map {it.inspections}.takeLast(2)
     println("Most active monkeys had $m1 and $m2 inspections. Answer: ${m1 * m2}")
+}
+
+fun performOperation(old: BigInteger, op: List<String>): BigInteger {
+    val operand = op[1]
+    val num1 = if (op[0] == "old") old else op[0].toBigInteger()
+    val num2 = if (op[2] == "old") old else op[2].toBigInteger()
+    return if (operand == "*") {
+        num1.multiply(num2)
+    } else {
+        num1.add(num2)
+    }
+}
+
+fun parseInput(input: List<String>): List<Monkey> {
+    val monkeyList: MutableList<Monkey> = mutableListOf()
+    var monkeyStartParse = 0
+    // Each monkey has 6 lines, followed by blank line
+    while (monkeyStartParse < input.size) {
+        // get start list
+        val startItems = input[monkeyStartParse + 1].split(": ")[1].split(", ").map{it.toBigInteger()}.toMutableList()
+        val operation = input[monkeyStartParse + 2].split(" = ")[1]
+        val divisibleByTest = input[monkeyStartParse+3].substringAfterLast(" ").toInt()
+        val ifTrueThrow = input[monkeyStartParse+4].substringAfterLast(" ").toInt()
+        val ifFalseThrow = input[monkeyStartParse+5].substringAfterLast(" ").toInt()
+        monkeyList.add(Monkey(ArrayDeque(startItems), operation, divisibleByTest, ifTrueThrow, ifFalseThrow))
+        monkeyStartParse += 7
+    }
+
+    return monkeyList
 }
 
 fun main(){
